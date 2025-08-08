fc59261ff083d1c071d0201bafa11d0c8f99c86c
adam-f-kennedy <afk05.artorias@gmail.com>
2025-08-08 23:47:59 +0100

    adam branch

--- Game/Levels/DemoWorld.lean
+++ Game/Levels/DemoWorld.lean
@@ -1,7 +1,7 @@
 import Game.Levels.DemoWorld.L01_HelloWorld
 
-World "DemoWorld"
-Title "Demo World"
+World "TutorialWorld"
+Title "Tutorial World"
 
 Introduction "
 This introduction is shown before one enters level 1 of the demo world. Use markdown.
