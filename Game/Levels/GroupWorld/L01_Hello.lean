a9686a9a71eb6ea3fe4fcbfc1f9284a62bca597f
AxBrown-122 <alexanderbrwn23@outlook.com>
2025-08-13 15:02:13 +0100

    axiom documentation

--- Game/Levels/GroupWorld/L01_Hello.lean
+++ Game/Levels/GroupWorld/L01_Hello.lean
@@ -17,19 +17,29 @@ variable {G : Type} [MyGroup G]
 open MyGroup -- for easy access to axioms
 
 /--
-This is some sample documentation - (test)
+This result states that for elements $a, x, y ∈ G $, if a * x = a * y then x = y.
 -/
 TheoremDoc mygroup.mul_left_cancel as "mul_left_cancel" in "Group"
 /--
-Associativity is cool!
+This Axiom states that our binary operation has the following property:
+ $g * (h * k)  = (g * h) * k$ for all $g, h, k ∈ G$.
+Then we say that the binary operation * is associative
 -/
 TheoremDoc MyGroup.mul_assoc as "mul_assoc" in "Axioms"
 /--
-Inverses are cool!
+This Axiom states that for every element $g ∈ G$, there exists an inverse element $g⁻¹ ∈ G$
+such that $g⁻¹ * g = e$ (where e is the identity element in G)
+
+## Important note
+This axiom specifiys that the inverse is applied multiplicativly on the LEFT, hence mul_left_inv
 -/
 TheoremDoc MyGroup.mul_left_inv as "mul_left_inv" in "Axioms"
 /--
-Identitys are cool!
+This Axiom states that there exists an element $e ∈ G$ which has the following property:
+$e * g = g $ for all $g ∈ G$
+
+## Important note
+This element $e$ is denoted as $1$ in the editor
 -/
 TheoremDoc MyGroup.one_mul as "one_mul" in "Axioms"
 
