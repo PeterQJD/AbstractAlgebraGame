14df9f7435e0a5dbaedfac8879a505c7ff407a1c
adam-f-kennedy <afk05.artorias@gmail.com>
2025-08-16 20:31:42 +0100

    tut L01 fix

--- Game/Levels/TutorialWorld/L03_induction.lean
+++ Game/Levels/TutorialWorld/L03_induction.lean
@@ -5,15 +5,15 @@ Level 3
 
 Title "Indopent element"
 
-Introduction "We will now prove a simple result that if $e^{2} = e where e \\in \\N$
+Introduction "We will now prove a simple result that if $e^{2} = e$ where $e \\in \\N$
 then $e^{n} = e$.
 
 This will be the last level of the tutorial world where you will be introduced
 to the intro and induction tactics.
 
 intro is used when there is a expression of the form $∀ (x : t), u$ or $t → u$
-in the former intro x puts the variable (x : t) into the local context and
-makes the new subgoal u in the latter it puts (h : t) in the local context
+in the former intro $x$ puts the variable $(x : t)$ into the local context and
+makes the new subgoal $u$ in the latter it puts $(h : t)$ in the local context
 and makes u the new subgoal.
 
 induction is a tactic that will perform induction on a variable in the local context
@@ -22,10 +22,11 @@ genral case.
 "
 universe u
 
-private def power {α : Type u} (mul : α → α → α) (t : α) (n : Nat) : α :=
+def power {α : Type u} (mul : α → α → α) (t : α) (n : Nat) : α :=
   match n with
   | 0     => t
   | n + 1 => power mul (mul t t) n
+
 
 Statement {α : Type} (M : MyCommMagma α) (h₁ : mul e e = e) :
   ∀ n : Nat, power mul e n = e  := by
@@ -33,6 +34,7 @@ Statement {α : Type} (M : MyCommMagma α) (h₁ : mul e e = e) :
 
     induction n
     rw [power]
+
     rw [power]
     rw [h₁]
     rw [n_ih]
@@ -48,4 +50,4 @@ Conclusion "Good Work!"
 
 /- Use these commands to add items to the game's inventory. -/
 
-NewTactic rw rfl induction power
+NewTactic intro rw rfl induction
