65db8db5a24ef849a5a62d6e63c6c6521fa460c3
PeterQJD <pqjd@proton.me>
2025-08-20 09:51:57 +0100

    Level 1 of SemiGroupWorld: basic associativity problem

--- Game/Levels/SemiGroupWorld/L01_Hello.lean
+++ Game/Levels/SemiGroupWorld/L01_Hello.lean
@@ -1,7 +1,7 @@
 import Game.Metadata
-import Game.CustomStructure.MyGroup
+import Game.CustomStructure.MySemigroup
 
-World "SemiGroupWorld"
+World "SemigroupWorld"
 Level 1
 Title "Hello World"
 
@@ -9,9 +9,17 @@ Introduction "
 A message shown at the beginning of the level. Use it to explain any new concepts.
 "
 
-/-- The exercise statement in natural language using latex: $\iff$. -/
-Statement (n : Nat) : 0 + n = n := by
-  sorry
+namespace mysemigroup
+
+variable {G : Type} [MySemigroup G]
+open MySemigroup -- for easy access to axioms
+
+/-This axiom tells us that multiplication in a semigroup is associative.-/
+TheoremDoc MySemigroup.mul_assoc as "mul_assoc" in "Axioms"
+
+/-This theorem shows that associativity can be extended to a product of four elements.-/
+Statement (a b c d : G) : (a * (b * (c * d))) = a * ((b * c) * d):= by
+  rw [mul_assoc]
 
 Conclusion "
 The message shown when the level is completed
