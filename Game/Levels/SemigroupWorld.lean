fd937cce77c062a43990555fbb13971a212b57f3
PQJD <pqjd@proton.me>
2025-08-21 11:09:09 +0100

    Added MySemigroup defintion and Level 1 of Semigroup World

---
+++ Game/Levels/SemigroupWorld.lean
@@ -0,0 +1,15 @@
+import Game.Levels.SemigroupWorld.L01_Hello
+
+World "SemigroupWorld"
+
+Title "Semigroup World"
+
+Introduction
+"
+# Welcome to Semigroup World!
+
+The tactics used will be predominatley ... .
+
+### click start to begin the first level!
+
+"
