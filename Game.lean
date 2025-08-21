b4e21721a50ef46af2c3b19d36fdacf67484698f
Alexander Brown <alexanderbrwn23@outlook.com>
2025-08-21 12:25:16 +0100

    fix

--- Game.lean
+++ Game.lean
@@ -1,7 +1,7 @@
 import GameServer.Commands
 import Game.Levels.TutorialWorld
 import Game.Levels.GroupWorld
-import Game.Levels.SemigroupWorld
+import Game.Levels.SemiGroupWorld
 import Game.Levels.InverseSemiGroupWorld
 import Game.Levels.MonoidWorld
 
