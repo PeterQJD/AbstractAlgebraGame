2b3afed177b13f2f287d484a8f361869c0ae4923
Alexander Brown <alexanderbrwn23@outlook.com>
2025-09-09 19:10:27 +0000

    branching and hint in L02 semigroupworld

--- Game/Levels/MonoidWorld.lean
+++ Game/Levels/MonoidWorld.lean
@@ -8,9 +8,13 @@ Title "Monoid World"
 
 Introduction
 "
-# Welcome to Monoid World!
+## Welcome to Monoid World!
 
-The tactics used will be predominatley ... .
+A monoid is a semigroup with the addeition of an identity element,
+which is an element of the set which when applied to any element of the set,
+leaves it unchanged, it is effectivley the 'do nothing' elment.
+
+The tactics used will be predominatley rewrites.
 
 ### click start to begin the first level!
 
--- Game/Levels/SemigroupWorld/L02_assocprob1.lean
+++ Game/Levels/SemigroupWorld/L02_assocprob1.lean
@@ -18,6 +18,13 @@ open MyStructure -- for easy access to axioms
 /--This theorem shows that associativity can be extended to a product of four elements.-/
 Statement (a b c d : S) : (a * (b * c)) * d = (a * b ) * (c * d):= by
   rewrite [mul_assoc]
+  Branch
+    symm
+    rewrite [mul_assoc]
+    rewrite [← mul_assoc b]
+    rfl
+  Hint "If you want to be precise with where you want to apply 'mul_assoc',
+   specify the variable after the statement, e.g rewrite [mul_assoc x] "
   rewrite [mul_assoc b]
   rewrite [← mul_assoc a]
   rfl
