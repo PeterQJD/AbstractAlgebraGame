df10fc6717e6421778169bbb309fcf0886d73b14
Alexander Brown <alexanderbrwn23@outlook.com>
2025-09-11 14:13:46 +0000

    tutorial world tested

--- Game/Levels/TutorialWorld/L05_nth_rewrite.lean
+++ Game/Levels/TutorialWorld/L05_nth_rewrite.lean
@@ -5,15 +5,18 @@ Level 5
 
 Title "The nth_rewrite tactic"
 
-Introduction ""
+Introduction "Click on the description box to get an idea of the tactic,
+each variable is index going left to right starting from 1, nth_rewrite allows you to
+specify exactly where in the expression you want to perform a rewrite by specifying the index.
+for example, 'nth_rewrite 2 [h]' would substitute at the second variable (going left to rigth) -
+provided the substitution is valid."
 
 namespace tutorial
 
-Statement (x y z : Nat) (h₁ : y = x) (h₂ : x * x * y = x * y * z) : x * y * y = x * y * z := by
-  nth_rewrite 1 [h₁]
+Statement (x y z : Nat) (h₁ : x = y) (h₂ : x * x * y = x * y * z) : x * y * y = x * y * z := by
+  Hint "Try nth_rewriting (using a specific index) at h₂"
+  nth_rewrite 2 [h₁] at h₂
   exact h₂
-
-
 
 Conclusion "Well done!
 
