5c9c5be7bf355a7eb7283bd8c97e88bc3653302a
Alexander Brown <alexanderbrwn23@outlook.com>
2025-09-11 07:22:28 +0000

    done tweaking

--- Game/Custom/MyStructure/Definition.lean
+++ Game/Custom/MyStructure/Definition.lean
@@ -8,7 +8,7 @@ class MyStructure (S : Type u) extends Inv S, Mul S, One S :=
 (mul_right_inv : ∀ a : S, a * a⁻¹ = 1)
 (mul_comm : ∀ a b : S, a * b = b * a )
 
-/--An inverse of an element x in a semigroup S is some y ∈ S such that:-/
+/--An inverse of an element x in a semigroup S is some y ∈ S such that x * y * x = x ∧ y * x * y = y:-/
 def MyStructure.weak_inverse {S : Type u} [MyStructure S] (x y : S) : Prop :=
 x * y * x = x ∧ y * x * y = y
 
