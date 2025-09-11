e6c2d2a0053b6e40b4a60b88cfd9f4c950c76658
Alexander Brown <alexanderbrwn23@outlook.com>
2025-09-11 07:38:21 +0000

    fixing spelling mistakes

--- Game/Levels/InverseSemiGroupWorld/L03_Hello.lean
+++ Game/Levels/InverseSemiGroupWorld/L03_Hello.lean
@@ -19,8 +19,9 @@ open MyStructure mysemigroup
 
 
 
-/-- This statement states that if weak inverse of a is b, then a * b is idempotent -/
+/-- This statement states that if the weak inverse of a is b, then a * b is idempotent -/
 Statement (a b : ISG) (h: weak_inverse a b) : Idempotent (a * b) := by
+  Hint (hidden := true) "You may want to rewrite using the definitions"
   rewrite [Idempotent_def]
   rewrite [weak_inverse_def] at h
   rewrite [← mul_assoc]
@@ -31,7 +32,7 @@ Statement (a b : ISG) (h: weak_inverse a b) : Idempotent (a * b) := by
 
 Conclusion "
 Now that we have an understanding of what a weak inverse is, as we progress
-into group would you will find the idea of an inverse changes, now an inverse of
+into group world you will find the idea of an inverse changes, now an inverse of
 a group element a is denoted a⁻¹, and a * a⁻¹ = a⁻¹ * a = e = 1, where e is the identity element
 denoted as 1.
 "
