a4fb0ea0220f752fcf963ec24c0e74b1b63b624f
Peter Davidson <pqjd@proton.me>
2026-03-12 07:46:14 +0000

    Editing MyNat

--- Game/Custom/MyNat/Addition.lean
+++ Game/Custom/MyNat/Addition.lean
@@ -7,5 +7,5 @@ opaque add : MyNat → MyNat → MyNat
 instance instAdd : Add MyNat where
   add := MyNat.add
 
-axiom add_zero (a : MyNat) : a + 0 = 0
+axiom add_zero (a : MyNat) : a + 0 = a
 axiom add_succ (a b : MyNat) : a + succ (b) = succ (a + b)
