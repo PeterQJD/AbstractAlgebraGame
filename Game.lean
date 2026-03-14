36239c4aa11fa0ec9e41e4b6cd0c7bb67bfb7459
Peter Davidson <pqjd@proton.me>
2026-03-14 13:41:44 +0000

    Minor edit to Game.lean

--- Game.lean
+++ Game.lean
@@ -1,6 +1,10 @@
 import GameServer.Commands
 import Game.Levels.TutorialWorld
 import Game.Levels.SemigroupWorld
+--import Game.Levels.MonoidWorld
+--import Game.Levels.InverseSemiGroupWorld
+--import Game.Levels.GroupWorld
+
 import I18n
 
 Game "MyGame"
